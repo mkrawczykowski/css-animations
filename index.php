e7165f7109b949d17de0916c206a78c1aca37d55
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-06-30 22:05:24 +0200

    Webpack

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  get_header();
+?>
+
+<div class="posts">
+  <button class="posts__button" id="posts__button">Wczytaj posty</button>
+  <div class="posts__list" id="posts__list"></div>
+</div>
+
+<?php
+  get_footer();
+?>
